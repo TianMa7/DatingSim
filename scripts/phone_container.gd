69f058c9b670b3cad3ea9aca77c748585db023ce
Archit Pandit <architpandit90@gmail.com>
2025-10-04 22:23:41 -0400

    start response functionality

---
+++ scripts/phone_container.gd
@@ -0,0 +1,29 @@
+extends VBoxContainer
+
+@export var chatContainer : VBoxContainer
+@export var buttonContainer : HBoxContainer
+
+signal on_response(responseID : int)
+
+var responding : bool = true
+
+func _process(_delta : float) -> void:
+	buttonContainer.visible = responding
+
+func _on_message_1_down() -> void:
+	add_chat("Message 1", 1)
+
+func _on_message_2_down() -> void:
+	add_chat("Message 2", 2)
+
+func _on_message_3_down() -> void:
+	add_chat("Message 3", 3)
+
+func add_chat(message : String, id : int) -> void:
+	var chat = Label.new()
+	
+	chat.text = message
+	chatContainer.add_child(chat)
+	
+	responding = false
+	on_response.emit(id)
---
+++ scripts/response_manager.gd
@@ -0,0 +1,4 @@
+extends ScrollContainer
+
+func _on_response(responseID: int) -> void:
+	pass # Replace with function body.
